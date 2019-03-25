4743c292d96486f6428d5dacd3a489975632b7cf
uo258014 <47207829+uo258014@users.noreply.github.com>
2019-03-25 10:37:37 +0100

    new Visitor

---
+++ DLP_1/src/visitor/TypeCheckingVisitor.java
@@ -0,0 +1,9 @@
+package visitor;
+
+import ast.expressions.IntLiteral;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.IntType;
+
+public class TypeCheckingVisitor extends ConcreteVisitor{
+
+
+}
